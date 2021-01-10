748f767b0a51a7a2bbabfae6808f7a85d50e1504
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-01-10 21:00:37 +0700

    Add files via upload

--- application/views/loginRegisterView.php
+++ application/views/loginRegisterView.php
@@ -33,29 +33,58 @@
         
     </div>
 </nav>
-<div class="container my-md-2">
-    <div class="row">
-        <form action="<?php echo base_url("userCon/login"); ?>" method="post">
-            <table>
-                <tr>
-                    <td>Email or Phone Number</td>
-                    <td><input class="form-control" type="text" name="auth"></td>
-                </tr>
-                <tr>
-                    <td>password</td>
-                    <td><input class="form-control" type="text" name="p"></td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center"> <input class="btn btn-success" type="submit" value="Login"></td>
-                </tr>
-            </table>
+<div class="container mt-md-5">
+    <div class="row mt-2" >
+        <div class="col-md-6" style="background-color:white; ">
+            <h1 class="my-3">Login</h1>
+            <form action="<?php echo base_url("userCon/login"); ?>" method="post" name="loginform">
+                <label for="auth">Email or Phone Number</label>                    
+                <input class="form-control mb-2" type="text" name="auth" id="auth" placeholder="Email / Phone Number">
+                <label for="p">password</label>   
+                <input class="form-control" type="password" name="p" id="p">
+                <input class="btn btn-success btn-block my-3" type="submit" value="Login">
+            </form>
+        </div>
+        <div class="col-md-6" style="background-color:white;" >
+            <h1 class="my-3">Register</h1>
+            <form action="#" method="post" name="Registerform">
+                <table class="table table-borderless">
+                    <tr>
+                        <td><b>Firstname</b> <label style="color:red">*</label></td>
+                        <td><input class="form-control mb-3" type="text" name="firstname" placeholder="Enter Firstname"></td>
+                        <td><input class="form-control mb-3" type="text" name="lastname" placeholder="Enter Lastname"></td>
+                    </tr>
+                    <tr>        
+                        <td ><b>Email</b> <label style="color:red">*</label></td>
+                        <td colspan="2"><input class="form-control mb-3" type="email" name="emailfield"  placeholder="Enter Email"></td>
+                        
+                    </tr>
+                    <tr>
+                        <td><b>Password</b> <label style="color:red"> * </label></td>
+                        <td  colspan="2"><input class="form-control mb-3" type="password" id="passwordfield" name="passwordfield" placeholder="Enter Password"></td>
+                        </tr>
+                        <tr>
+                            <td><b>Confirm Password</b> <label style="color:red"> * </label></td>
+                            <td  colspan="2"><input class="form-control mb-3" type="password" name="confirmpasswordfield" placeholder="Enter Password Again"></td>
+                            </tr>   
+                        <tr>
+                            <td><b>Address</b> (Optional)</td>
+                            <td  colspan="2">
+                                <textarea class="form-control mb-3" rows="3" name="address"></textarea>
+                            </td>
+                        </tr>
+
+                </table>
+
+                <input class="btn btn-success btn-block my-3" type="submit" value="Register">
+            </form>
+            
+        </div>
             
            
-        </form>
+       
     </div>
-    <div class="row">
-        <img src="">
-    </div>
+    
 
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
