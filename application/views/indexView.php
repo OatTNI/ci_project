0626a8c50182eaa3746c3f1d0665d7e1f828e937
Jiramed-Jetawattana <76511634+Jiramed-Jetawattana@users.noreply.github.com>
2021-01-10 21:32:16 +0700

    Add files via upload

--- application/views/indexView.php
+++ application/views/indexView.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/e7d4730b9e.js" crossorigin="anonymous"></script>
     <style type = "text/css">
-        body{font-family:"Kanit", sans-serif !important;}
-        a { text-decoration: underline; }
+         body{
+            font-family: 'Kanit', sans-serif;
+        }
     </style>
 </head>
 
@@ -22,8 +22,8 @@
     <div class="container">
         <a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
             <i class="fas fa-shopping-basket">
-            เว็บขายของ
             </i>
+            เว็บขายของ
         </a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <form class="mx-2 my-auto d-inline w-100">
@@ -50,92 +50,176 @@
         </div>
     </div>
 </nav>
-<!-- แสดงรูปภาพ -->
-<div style="background-color:black;">
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner" style="height:200px;">
-    <div class="carousel-item active">
-      <img src="https://image.freepik.com/free-photo/milky-way_1401-347.jpg" class="d-block w-100" alt="...">
+<!-- แสดงรูปและหมวดหมู่ -->
+<div class="row mx-5 mt-3">
+    <div class="col-sm-6 col-md-8">
+        <!-- แสดงรูป -->
+        <div id="Product" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner rounded">
+              <div class="carousel-item active">
+                <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                class="d-block w-100" alt="..."  style="height: 500px; width: auto;">
+              </div>
+              <div class="carousel-item">
+                <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                class="d-block w-100" alt="..."  style="height: 500px; width: auto;">
+              </div>
+              <div class="carousel-item">
+                <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                class="d-block w-100" alt="..."  style="height: 500px; width: auto;">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#Product" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#Product" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+        </div>
     </div>
-    <div class="carousel-item">
-      <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="https://image.freepik.com/free-photo/milky-way_1401-347.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
-<!-- แสดงสินค้า -->
-<div class="card container my-2 border-0 bg-light" style="background-color:#F5F6FA;">
-<h4>หมวดหมู่</h4>
-    <div class="row">
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
-        <div class="col text-center">
-        <a href="#" > 
-        <img class="card-img-top" src="https://drive.google.com/uc?id=1GIEamQkYK55EGyt7eJA2X_4nwRPTIKn5" 
-        alt="IT" height="100" >
-        <span>IT</span>
-        </a>
-        </div>
+    <div class="col-6 col-md-4">
+        <!-- หมวดหมู่สินค้า -->
+        <div class="card ">
+            <div class="card-header">
+              หมวดหมู่สินค้า
+            </div>
+            <ul class="list-group list-group-flush">
+              <a href="#" class="text-dark"><li class="list-group-item">คอมพิวเตอร์และสินค้า IT</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">เกมและของเล่น</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">กล้องและอุปกรณ์ถ่ายภาพ</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">กีฬาและการเดินป่า</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">เสื้อผ้าชาย</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">เสื้อผ้าหญิง</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">อาหารเสริมและสุขภาพ</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">รองเท้าหญิง</li></a>
+              <a href="#" class="text-dark"><li class="list-group-item">รองเท้าชาย</li></a>
+            </ul>
+          </div>
     </div>
 </div>
+<!--แสดงสินค้าแบบย่อ ๆ 1-->
+<div class="row mx-5 mt-5 mb-2">
+    <div class="col">
+        <div class="card border-secondary border-0">
+            <div class="card-header border-0">
+                <div class="row no-gutters">
+                    <div class="col-sm-6 col-md-8">คอมพิวเตอร์และสินค้า IT</div>
+                    <div class="col-6 col-md-4 text-right"><a href="#">ดูทั้งหมด</a></div>
+                </div>
+            </div>
+            <div class="card-body text-secondary text-center border-0">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://img.advice.co.th/images_nas/pic_product4/A0129858/A0129858_1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://img.advice.co.th/images_nas/pic_product4/A0129858/A0129858_1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://img.advice.co.th/images_nas/pic_product4/A0129858/A0129858_1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://img.advice.co.th/images_nas/pic_product4/A0129858/A0129858_1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+</div>
+<!--แสดงสินค้าแบบย่อ ๆ 2-->
+<div class="row mx-5 mt-3 mb-2">
+    <div class="col">
+        <div class="card border-secondary border-0">
+            <div class="card-header border-0">
+                <div class="row no-gutters">
+                    <div class="col-sm-6 col-md-8">กล้องและอุปกรณ์ถ่าย</div>
+                    <div class="col-6 col-md-4 text-right"><a href="#">ดูทั้งหมด</a></div>
+                </div>
+            </div>
+            <div class="card-body text-secondary text-center border-0">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://www.zoomcamera.net/wp-content/uploads/2019/09/Nikon-COOLPIX-P1000-Digital-Camera-1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://www.zoomcamera.net/wp-content/uploads/2019/09/Nikon-COOLPIX-P1000-Digital-Camera-1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://www.zoomcamera.net/wp-content/uploads/2019/09/Nikon-COOLPIX-P1000-Digital-Camera-1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://www.zoomcamera.net/wp-content/uploads/2019/09/Nikon-COOLPIX-P1000-Digital-Camera-1.jpg" class="card-img-top" alt="คอมไง">
+                            <div class="card-body">
+                                <p class="card-text">
+                                    ข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูลข้อมูล
+                                </p>
+                            </div>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>        
+    </div>
+</div>
+<!-- footer -->
+<footer class="bg-dark text-light text-center text-lg-start mt-4 border-top border-dark">
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+      CI Framework Project 
+      <a class="text-light" href="https://www.tni.ac.th/home/">Thai-Nichi Institute of Technology</a>
+    </div>
+</footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
