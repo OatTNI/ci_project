864d06a887e758a9b8ceebfa05a2d5343f5f0858
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-01-12 10:28:39 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -6,17 +6,18 @@
             เว็บขายของ
         </a>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <form class="mx-2 my-auto d-inline w-50">
-            <div class="input-group">
-                <input type="search" class="form-control" placeholder="Search...">
-                <span class="input-group-append">
-                <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
-                </span>
+            <div class="navbar-nav mr-auto col-md-9">
+                <form class="mx-2 my-auto d-inline w-100">
+                    <div class="input-group">
+                        <input type="search" class="form-control" placeholder="Search...">
+                        <span class="input-group-append">
+                            <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                        </span>
+                    </div>
+                </form>
             </div>
-        </form>
-        <div class="navbar-nav mr-md-auto">
-            <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i><span class='minitext'> ตะกร้าสินค้า</span></a></li>
-            <?php if($lnstatus==0){?> 
+            <div class="navbar-nav ml-auto col-md-3">
+                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i><span class='minitext'> ตะกร้าสินค้า</span></a></li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-user"></i><span class='minitext'> เข้าสู่ระบบ</span> </a>
@@ -24,11 +25,9 @@
                         <a class="dropdown-item" href="<?php echo base_url("userCon/index"); ?>">Login</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Logout</a>
-                    
                     </div>
                 </li>
-            <?php } ?>
-
+            </div>
         </div>
     </div>
 </nav>
