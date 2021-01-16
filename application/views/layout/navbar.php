cb00c8e550e14dd66eee7bfb24a54cdecfb7cbc6
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-01-16 15:19:45 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,33 +1,35 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
-            <i class="fas fa-shopping-basket">
-            </i>
-            เว็บขายของ
-        </a>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav mr-auto col-md-9">
-                <form class="mx-2 my-auto d-inline w-100">
-                    <div class="input-group">
-                        <input type="search" class="form-control" placeholder="Search...">
-                        <span class="input-group-append">
-                            <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
-                        </span>
+        <div class="col-md-2"><a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
+            <i class="fas fa-shopping-basket"></i>เว็บขายของ</a>
+        </div>  
+        <div class="col-10 ">
+            <div class="row flex-column">
+                <div class="col-md-2 offset-md-10 offset-8">
+                    <div class="navbar-nav">
+                        <li class="nav-item"><span class='minitext'> เข้าสู่ระบบ </span> <i class="fas fa-user"></i> </a></li>
+
                     </div>
-                </form>
-            </div>
-            <div class="navbar-nav ml-auto col-md-3">
-                <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i><span class='minitext'> ตะกร้าสินค้า</span></a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user"></i><span class='minitext'> เข้าสู่ระบบ</span> </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="<?php echo base_url("userCon/index"); ?>">Login</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Logout</a>
+                </div>
+                <div class="col-10">
+                    <div class="navbar-nav">
+                        <form class="mx-2 my-auto d-inline w-100">
+                            <div class="input-group">
+                                <input type="search" class="form-control" placeholder="Search...">
+                                <span class="input-group-append"><button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button></span>
+                            </div>
+                        </form>
+                        
                     </div>
-                </li>
+                    <div class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a></li>
+                    </div>
+                </div>
+                
             </div>
-        </div>
+
+        </div>  
+        
+        
     </div>
 </nav>
