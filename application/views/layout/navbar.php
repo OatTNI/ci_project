537f5fc0b6bb075342abddf7a2167ad797144a2a
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-01-16 23:17:59 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,33 +1,39 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <div class="col-md-2"><a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
+        <div class="col-md-2 col-5"><a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
             <i class="fas fa-shopping-basket"></i>เว็บขายของ</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler1" aria-controls="navbarToggler1"  aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
         </div>  
-        <div class="col-10 ">
-            <div class="row flex-column">
-                <div class="col-md-2 offset-md-10 offset-8">
-                    <div class="navbar-nav">
-                        <li class="nav-item"><span class='minitext'> เข้าสู่ระบบ </span> <i class="fas fa-user"></i> </a></li>
+        
 
+        <div class="col-md-10 ">
+            <div class="collapse navbar-collapse flex-column" id="navbarToggler1" >
+            
+                <div class="col col-md-2 offset-md-10">
+                    <div class="navbar-nav"  >
+                        <li class="nav-item"><span class='minitext'> เข้าสู่ระบบ </span> <i class="fas fa-user"></i> </a></li>
+                        
                     </div>
                 </div>
-                <div class="col-10">
+                <div class="col col-md-10 mr-auto">
                     <div class="navbar-nav">
-                        <form class="mx-2 my-auto d-inline w-100">
+                        <form class=" my-auto d-inline w-100">
                             <div class="input-group">
                                 <input type="search" class="form-control" placeholder="Search...">
                                 <span class="input-group-append"><button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button></span>
+                                 
                             </div>
+
                         </form>
-                        
-                    </div>
-                    <div class="navbar-nav">
                         <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i></a></li>
                     </div>
+                   
                 </div>
                 
+          
             </div>
-
         </div>  
         
         
