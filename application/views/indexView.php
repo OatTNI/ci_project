c291a4aac31b2ebd636e8e716795a471edce7f0f
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-01-19 23:03:25 +0700

    index updated

--- application/views/indexView.php
+++ application/views/indexView.php
@@ -7,10 +7,112 @@
 </head>
 
 <body class="bg-light">
-
+    <!--navbar -->
     <?php $this->load->view('layout/navbar') ?>
-    <!-- แสดงรูปและหมวดหมู่ -->
-
+    <!-- content -->
+    <div class="row no-gutters p-4 pt-4 colorgrd">
+        <!--Big-->
+        <div class="col-8 no-gutters">
+            <!-- sale -->
+        <div id="Sale" class="carousel slide carousel-fade sizeimgbig" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#Sale" data-slide-to="0" class="active bg-warning"></li>
+                        <li data-target="#Sale" data-slide-to="1" class="bg-warning"></li>
+                        <li data-target="#Sale" data-slide-to="2" class="bg-warning"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://uicookies.com/wp-content/uploads/2019/07/ecommerce-website-template.jpg" 
+                            class="d-block w-100 sizeimgbig">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                            class="d-block w-100 sizeimgbig">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://dist.affiliates.envato.com/uicookies/BS+ecommerce+.png" 
+                            class="d-block w-100 sizeimgbig">
+                        </div>
+                    </div>
+                        <a class="carousel-control-prev" href="#Sale" role="button" data-slide="prev">
+                            <h1><i class="fas fa-chevron-left text-warning" aria-hidden="true"></i>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#Sale" role="button" data-slide="next">
+                            <h1><i class="fas fa-chevron-right text-warning" aria-hidden="true"></i>
+                            <span class="sr-only ">Next</span>
+                        </a>
+                </div>
+        </div>
+        <div class="col no-gutters">
+            <!--1-->
+            <div class="col no-gutters">
+                <!-- new -->
+                <div id="New" class="carousel slide carousel-fade size" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#New" data-slide-to="0" class="active bg-warning"></li>
+                        <li data-target="#New" data-slide-to="1" class="bg-warning"></li>
+                        <li data-target="#New" data-slide-to="2" class="bg-warning"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://uicookies.com/wp-content/uploads/2019/07/ecommerce-website-template.jpg" 
+                            class="d-block w-100 size">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                            class="d-block w-100 size">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://dist.affiliates.envato.com/uicookies/BS+ecommerce+.png" 
+                            class="d-block w-100 size">
+                        </div>
+                    </div>
+                        <a class="carousel-control-prev" href="#New" role="button" data-slide="prev">
+                            <h1><i class="fas fa-chevron-left text-warning" aria-hidden="true"></i>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#New" role="button" data-slide="next">
+                            <h1><i class="fas fa-chevron-right text-warning" aria-hidden="true"></i>
+                            <span class="sr-only ">Next</span>
+                        </a>
+                </div>
+            </div>
+            <!--2-->
+            <div class="col">
+                 <!-- Recent -->
+                <div id="Recent" class="carousel slide carousel-fade size" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#Recent" data-slide-to="0" class="active bg-warning"></li>
+                        <li data-target="#Recent" data-slide-to="1" class="bg-warning"></li>
+                        <li data-target="#Recent" data-slide-to="2" class="bg-warning"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://uicookies.com/wp-content/uploads/2019/07/ecommerce-website-template.jpg" 
+                            class="d-block w-100 size">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://thethaiger.com/wp-content/uploads/2018/05/39f9d5bd5ee7a1ecebfb8c206d0aa32e.jpg" 
+                            class="d-block w-100 size">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://dist.affiliates.envato.com/uicookies/BS+ecommerce+.png" 
+                            class="d-block w-100 size">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#Recent" role="button" data-slide="prev">
+                        <h1><i class="fas fa-chevron-left text-warning" aria-hidden="true"></i>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#Recent" role="button" data-slide="next">
+                        <h1><i class="fas fa-chevron-right text-warning" aria-hidden="true"></i>
+                        <span class="sr-only ">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+</div>  
     <div class="row mx-5 mt-3">
         <div class="col-sm-6 col-md-8">
             <!-- แสดงรูป -->
@@ -189,14 +291,16 @@
         </div>
     </div>
     <!-- footer -->
-    <footer class="bg-dark text-light text-center text-lg-start mt-4 border-top border-dark">
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            CI Framework Project
-            <a class="text-light" href="https://www.tni.ac.th/home/">Thai-Nichi Institute of Technology</a>
-        </div>
-    </footer>
+<footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: #EDF5E1">
+    © 2020 Copyright:
+    <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-</body>
-
+  </body>
 </html>
--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,21 +1,50 @@
-<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+<nav class="navbar sticky-top navbar-expand-md fornav navbar-light bg-light">
+    <div class="container pt-3 px-4 rounded">
+        <div class="d-flex flex-column align-items-start w-100">
+            <div class="input-group">
+                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand ml-lg-2 ml-3" href="#"><i class="fas fa-shopping-basket"></i> เว็บขายของ</a>
+                    <input class="form-control mr-2 rounded" type="Search" placeholder="Search" aria-label="Search">
+                    <button class="btn" type="submit" style="background-color:#05386B"><i class="fas fa-search text-light"></i></button>
+            </div>
+            <div class="collapse navbar-collapse ml-lg-0 ml-3" id="navbarCollapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo base_url("indexCon/index"); ?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Product</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> My Cart</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-user-circle"></i> My Account</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+
+
+
+<!-- <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <div class="col-md-2"><a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
+        <div class="col-2"><a class="navbar-brand" href="<?php echo base_url("indexCon/index"); ?>">
             <i class="fas fa-shopping-basket"></i>เว็บขายของ</a>
         </div>  
         
 
         <div class="col-md-10 " id="header-pc">
             <div class="row"  >
-            
-                <div class="col-10 col-md-5 offset-md-8 offset-2">
-                    <div class="navbar-nav"  >
-                        <li class="nav-item"><a class="nav-link" href="#"><span class='minitext'> ตะกร้าสินค้า </span><i class="fas fa-shopping-cart"></i></a> </li>
-                        <li class="nav-item"><a class="nav-link" href="#"><span class='minitext'> เข้าสู่ระบบ </span> <i class="fas fa-user"></i></a></li>
-                        
-                    </div>
-                </div>
-                <div class="col-12 col-md-10 mr-auto">
+              
+                <div class="col-10 col-md-8 mr-auto">
                     <div class="navbar-nav">
                         <form class=" my-auto d-inline w-100">
                             <div class="input-group">
@@ -29,6 +58,13 @@
                     </div>
                    
                 </div>
+                <div class="col-md-4 ml-auto" id="menu1div" >
+                    <div class="navbar-nav"  >
+                        <li class="nav-item"><a class="nav-link" href="#"><span class='minitext'> ตะกร้าสินค้า </span><i class="fas fa-shopping-cart"></i></a> </li>
+                        <li class="nav-item"><a class="nav-link" href="#"><span class='minitext'> เข้าสู่ระบบ </span> <i class="fas fa-user"></i></a></li>
+                        
+                    </div>
+                </div>
                 
           
             </div>
@@ -36,4 +72,4 @@
         
         
     </div>
-</nav>
+</nav> -->
