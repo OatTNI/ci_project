9b76dd77b2511987756027b5887fc612c0463133
Kaotto <tanutchakornkhaisaeng@gmail.com>
2021-01-21 21:23:29 +0700

    add ; after base_url()

--- application/views/layout/header-css.php
+++ application/views/layout/header-css.php
@@ -1,7 +1,6 @@
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ci project</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" 
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php echo base_url('assets/') ?>fontawesome-free-5.15.1/css/all.min.css">
-    <link href ="<?php echo base_url('assets/')?>myCSS.css" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ci project</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link rel="stylesheet" href="<?php echo base_url('assets/'); ?>fontawesome-free-5.15.1/css/all.min.css">
+	<link href="<?php echo base_url('assets/'); ?>myCSS.css" rel="stylesheet">
