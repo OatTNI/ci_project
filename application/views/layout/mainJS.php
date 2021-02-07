3aef84ab6eee70d2c5d19930b44fcf7f7acf5a06
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-02-07 17:12:24 +0700

    JS

--- application/views/layout/mainJS.php
+++ application/views/layout/mainJS.php
@@ -10,6 +10,7 @@
     <script src="<?php echo base_url('assets/') ?>js/jquery.mCustomScrollbar.concat.min.js"></script>
     <script src="<?php echo base_url('assets/') ?>js/custom.js"></script>
     <!-- javascript -->
+    <script src="<?php echo base_url('assets/'); ?>js/bootstrap-input-spinner.js"></script>
     <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
     <script>
         $(document).ready(function() {
