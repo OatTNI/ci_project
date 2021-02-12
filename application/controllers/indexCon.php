31155da28f29e5a18030706c23d787946bb624e4
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-12 22:37:28 +0700

    category session

--- application/controllers/indexCon.php
+++ application/controllers/indexCon.php
@@ -7,13 +7,18 @@ class indexCon extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Product_model');
-        // $this->load->model('Category_model');
+        $this->load->model('Category_model');
     }
     
     public function index($user_data = "")
     {
         $data["Pre"] = $this->Product_model->getProducts();
-        // $data["category"] = $this->Category_model->getCategories();
+
+        $category=$this->Category_model->getCategories();
+        $category=array(
+            'category'=>$category
+        );
+        $this->session->set_userdata($category);
         $data["user"] = $user_data;
         $this->load->view('indexView', $data);
     }
