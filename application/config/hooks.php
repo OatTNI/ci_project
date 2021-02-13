d50726b35dada15ba4682bb9429edd764fe77882
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-14 01:55:38 +0700

    add hook for login

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['post_controller_constructor'] = array(
+                                'class'    => 'loginHook',
+                                'function' => 'login_first',
+                                'filename' => 'loginHook.php',
+                                'filepath' => 'hooks',
+                                'params'=>array()
+                                );
