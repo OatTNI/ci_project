4309c9cd1ba90f381a266aa06f3823a660807e29
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-14 04:18:44 +0700

    update sql prevention

--- application/controllers/user/security.php
+++ application/controllers/user/security.php
@@ -1,13 +1,14 @@
 <?php
-
 /*
 * What: to prevent an sql injection attack
 * Author: oat
 * return: string
 */
 function sql_prevention($input){
+    $link = mysqli_connect("localhost", "root", "", "SC_FS");
     $input=trim($input);
     $input=htmlspecialchars($input);
-    $input=mysqli_escape_string($input);
+    $input=mysqli_escape_string($link,$input);
     return $input;
 }
+
