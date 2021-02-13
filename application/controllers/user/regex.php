b6408d2a8454a4087be2f17f2a624d2caba6a591
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-14 01:17:28 +0700

    add two  regex functions for phone and password

---
+++ application/controllers/user/regex.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+* What: regex for check phone pattern for login and validation form
+* Author: oat
+* return: string of regex
+*/
+function regex_phone(){
+    $pattern="/^0{1}[689]{1}\d{8}$/";
+    return $pattern;
+}
+
+/*
+* What: regex for check password on validation form
+* Author: oat
+* return: string of regex
+*/
+function regex_password(){
+    $pattern="/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$])[a-zA-Z\d!@#$]{8,}$/";
+    return $pattern;
+}
