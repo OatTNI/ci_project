e1148c4fbe35bc668ec27aa2716e89d8ec5d179d
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-02-17 00:07:59 +0700

    404 not found

---
+++ application/views/err404.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Page Not Found</title>
+</head>
+<body>
+	<div class="container-fluid">
+		<img src="<?php echo base_url("assets/images/exh404.jpg");?>">
+	</div>
+</body>
+</html>
