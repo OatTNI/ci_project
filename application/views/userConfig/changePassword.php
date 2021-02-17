117b94809925b48966722511c106c1ddeb646edc
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-17 09:14:04 +0700

    echo form_error

--- application/views/userConfig/changePassword.php
+++ application/views/userConfig/changePassword.php
@@ -1,3 +1,4 @@
+<?php print_r($this->session->flashdata('error'));?>
 <div class="container BGPanel my-2" >
   <div class="row">
     <div class="col HDText mt-2">
@@ -7,19 +8,25 @@
 <div class="dropdown-divider"></div>
    <div class="row " >
     <div class="col">
-      <form action="#?>" method="post" name="ChangePassword">
+      <form action="<?php echo base_url("user/password"); ?>" method="post" name="ChangePassword">
             <table class="table table-borderless"> 
               <tr>
                     <td><b>Old Password</b> <label style="color:red"> * </label></td>
-                    <td colspan="2"><input class="form-control mb-3" type="password" id="oldpasswordfield" name="oldpasswordfield" placeholder="Enter Old Password"></td>
+                    <td colspan="2">
+                    <?php echo form_error('oldpasswordfield'); ?>
+                    <input class="form-control mb-3" type="password" id="oldpasswordfield" name="oldpasswordfield" placeholder="Enter Old Password"></td>
                 </tr>
                  <tr>
                     <td><b>Password</b> <label style="color:red"> * </label></td>
-                    <td colspan="2"><input class="form-control mb-3" type="password" id="passwordfield" name="passwordfield" placeholder="Enter Password"></td>
+                    <td colspan="2">
+                    <?php echo form_error('passwordfield'); ?>
+                    <input class="form-control mb-3" type="password" id="passwordfield" name="passwordfield" placeholder="Enter Password"></td>
                 </tr>
                 <tr>
                     <td><b>Confirm Password</b> <label style="color:red"> * </label></td>
-                    <td colspan="2"><input class="form-control mb-3" type="password" name="confirmpasswordfield" placeholder="Enter Password Again"></td>
+                    <td colspan="2">
+                    <?php echo form_error('confirmpasswordfield'); ?>
+                    <input class="form-control mb-3" type="password" name="confirmpasswordfield" placeholder="Enter Password Again"></td>
                 </tr>
             </table>
         <input class="btn btn-success btn-block my-3" type="submit" value="Change Password">
