e2652465a9a1749365afa0aed7f250db018aeab3
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-18 01:29:43 +0700

    update index and others

--- application/controllers/user/information.php
+++ application/controllers/user/information.php
@@ -7,7 +7,9 @@ class information extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("userModel");   
+        $this->load->model("userModel");
+        require("regex.php");
+        require("security.php");
     }
 
 /*
@@ -15,19 +17,21 @@ class information extends CI_Controller {
 * Author: oat
 * return: return nothing
 */
-    public function index($method="")
+    public function index()
     {
-        if($method=="show"){
-            $data["content"] = "userConfig/MyInfo";
-	        $this->load->view('userView', $data);
-        }else{
             if($this->session->userdata("user_id")){
+                $this->set_all_rules();
+                if($this->form_validation->run()==FALSE){
+                    $data["content"] = "userConfig/MyInfo";
+	                $this->load->view('userView', $data);
+                }else{
+
+                }
             }
             else{
-                $user_data=getInfo($this->session->userdata("user_id"));
-                $this->load->view("");
+                redirect("Home/index");
+                
             }
-        }
     }
         
 /*
@@ -35,20 +39,97 @@ class information extends CI_Controller {
 * Author: oat
 * return: array of user information
 */
-    private function changeInfo(){
-
+    private function set_all_rules(){
+        $regex_phone=regex_phone();
+        $config =
+        [
+            [
+                // ! Firstname
+                "field"=>"firstname",
+                "label"=>"Firstname",
+                "rules"=>"required|alpha",
+                "errors"=>[
+                    "required"=>"your %s must be provided"
+                ]
+            ],
+            [
+                // ! LastName
+                "field"=>"lastname",
+                "label"=>"Lastname",
+                "rules"=>"required|alpha"
+            ],
+            [
+                // ! Email
+                "field"=>"emailfield",
+                "label"=>"Email",
+                "rules"=>"required|valid_email"
+            ],
+            [
+                // ! Phone Number
+                "field"=>"phonenumber",
+                "label"=>"Phone Number",
+                "rules"=>"required|numeric|exact_length[10]|regex_match[{$regex_phone}]"
+            ],
+            [
+                // ! address
+                "field"=>"address",
+                "label"=>"address",
+                "rules"=>"required"
+            ]
+            
+        ];
+        $this->form_validation->set_rules($config);
     }
-    private function getInfo($user_id){
-        $temp1=$this->userModel->get_user_by_id($user_id);
-        $temp2=[];
-        foreach($temp1 as $row){
-            $temp2[0]=$row->first_name;
-            $temp2[1]=$row->last_name;
-            $temp2[2]=$row->email;
-            $temp2[3]=$row->mobile;
-            $temp2[4]=$row->address;
+
+/*
+* What: use to get post data for form validation
+* Author: oat
+* return: array of post data
+*/
+    private function get_all_post_data(){
+        $fname=$this->input->post("firstname");
+        $lname=$this->input->post("lastname");
+        $email=$this->input->post("email");
+        $phone=$this->input->post("phonenumber");
+        $addr=$this->input->post("address");
+
+        $fname=sql_prevention($fname);
+        $lname=sql_prevention($lname);
+        $email=sql_prevention($email);
+        $phone=sql_prevention($phone);
+        $addr=sql_prevention($addr);
+
+        return [$fname,$lname,$email,$phone,$addr];
+    }
+
+/*
+* What: use to check Duplicate email in user table
+* Author: oat
+* return: boolean
+*/
+    private function isDuplicateEmail($email){
+        $temp=$this->userModel->get_email();
+        foreach($temp as $row){
+            if($row->email==$email){
+                return true;
+            }
         }
-        return $temp2;
+        return false;
+    }
+
+/*
+* What: use to check Duplicate phone number in user table
+* Author: oat
+* return: boolean
+*/
+    private function isDuplicatePhone($phone){
+        $temp=$this->userModel->get_mobile();
+        foreach($temp as $row){
+            if($row->mobile==$phone){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
