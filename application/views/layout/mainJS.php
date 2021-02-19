39bd6a670f9d357c350f1800265234873f3ef6fe
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-02-19 22:33:17 +0700

    <script src="<?php echo base_url('assets/') ?>js/jquery.min.js"></script>

--- application/views/layout/mainJS.php
+++ application/views/layout/mainJS.php
@@ -1,7 +1,7 @@
  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
  <!-- Javascript files-->
- <script src="<?php echo base_url('assets/') ?>js/jquery.min.js"></script>
+ <!-- <script src="<?php /*echo base_url('assets/') */?>js/jquery.min.js"></script> -->
  <script src="<?php echo base_url('assets/') ?>js/popper.min.js"></script>
  <script src="<?php echo base_url('assets/') ?>js/bootstrap.bundle.min.js"></script>
  <script src="<?php echo base_url('assets/') ?>js/jquery-3.0.0.min.js"></script>
