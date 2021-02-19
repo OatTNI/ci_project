60c93e4f227918015f621fdfe38c39afbc8cbdfd
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-02-19 22:32:52 +0700

    เพื่อชื่อผู้ผลิต

--- application/views/Admin/Management/ItemManager.php
+++ application/views/Admin/Management/ItemManager.php
@@ -18,6 +18,7 @@
 	            <div class="card-body">
 	                <h5 class="card-title text-truncate"><strong><?php echo $P->product_name; ?></strong></h5>
 	                <p class="card-text text-truncate"><?php echo $P->description; ?></p>
+					<p class="card-text text-truncate"><?php echo $P->vendor; ?></p>
 	                <a href="#" class="btn btn-warning">แก้ไข</a>
 	                <a href="#" class="btn btn-danger">ลบ</a>
 	            </div>
