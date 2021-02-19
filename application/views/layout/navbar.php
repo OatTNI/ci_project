97df159f6d4d1d6bfcae2265b9e3af3cc5c12f17
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-02-20 00:26:59 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -1,4 +1,4 @@
- <?php $category=$this->session->userdata('category'); ?>
+ <?php $category = $this->session->userdata('category');?>
  <!-- loader  -->
  <!--    <div class="loader_bg">
         <div class="loader"><img src="<?php// echo base_url('assets/') ?>images/loading.gif" alt="#"></div>
@@ -30,35 +30,39 @@
                          </div>
                      </div>
                  </div>
-                    <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-                        <div class="input-group">
-                        <!-- Search -->
-                            <input class="form-control mr-2 rounded"  type="text" id="keyword" name="keyword" value="" placeholder="What are you looking for?" aria-label="Search">                        
-                                  
-                        <button class="btn btn-lg rounded-circle" onclick="FuntionSearch()" style="background-color:#05386B" role="button" ><i class="fas fa-search text-light"></i></button>
+                 <div class="col-xl-9 col-lg-9 col-md-9 col-sm-9">
+                     <div class="input-group">
+                         <!-- Search -->
+                         <input class="form-control mr-2 rounded" type="text" id="keyword" name="keyword" value="" placeholder="What are you looking for?" aria-label="Search">
+
+                         <button class="btn btn-lg rounded-circle" onclick="FuntionSearch()" style="background-color:#05386B" role="button"><i class="fas fa-search text-light"></i></button>
                          <!-- Cart -->
-                            <a class="btn btn-lg btn-primary-beautyful ml-2" href="<?php echo base_url("cart/Home"); ?>" role="button"><i class="fas fa-shopping-cart"></i> My Cart</a>
-                        </div>
-                    </div>
+                         <a class="btn btn-lg btn-primary-beautyful ml-2" href="<?php echo base_url("cart/Home"); ?>" role="button"><i class="fas fa-shopping-cart"></i> My Cart</a>
+                     </div>
+                 </div>
                  <!-- Menu Bar -->
                  <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
                      <div class="menu-area mt-2">
                          <div class="limit-box">
                              <nav class="main-menu">
                                  <ul class="menu-area-main">
-                                    <li> <a class="text-dark" href="<?php echo base_url("Home/index"); ?>">Home</a> </li>
-                                    <li> <a class="text-dark" href="<?php echo base_url("Shop/index"); ?>">Shop</a></li>
-                                    <li> <a class="text-dark" href="#" onclick="openNav()">Category</a></li>
-                                    <?php 
-                                    if(!$this->session->userdata("user_id")){ ?>
-                                        <li> <a class="text-dark" href="<?php echo base_url("user/login_out/index/show"); ?>">Login</a></li>
+                                     <li> <a class="text-dark" href="<?php echo base_url("Home/index"); ?>">Home</a> </li>
+                                     <li> <a class="text-dark" href="<?php echo base_url("Shop/index"); ?>">Shop</a></li>
+                                     <li> <a class="text-dark" href="#" onclick="openNav()">Category</a></li>
+                                     <?php
+                                        if (!$this->session->userdata("user_id")) { ?>
+                                         <li> <a class="text-dark" href="<?php echo base_url("user/login_out/index/show"); ?>">Login</a></li>
 
-                                    <?php } 
-                                    else{
-                                    ?>
-                                        <li> <a class="text-dark" href="<?php echo base_url("myAccount/index"); ?>"><i class="fas fa-user-circle"></i> <?php echo $this->session->userdata("user_fname"); ?></a></li>
-                                        <li> <a class="text-dark" href="<?php echo base_url("user/login_out/index/"); ?>">Logout</a></li>                                  
-                                    <?php } ?>                              
+                                         <?php } else {
+                                             
+                                            if ($this->session->userdata("user_status") == 1) { ?>
+                                             <li> <a class="text-dark" href="<?php echo base_url("admin/index"); ?>"><i class="fas fa-user-circle"></i>ไปสู่แสงสว่าง</a></li>
+                                             <li> <a class="text-dark" href="<?php echo base_url("user/login_out/index/"); ?>">Logout</a></li>
+                                         <?php } else { ?>
+                                             <li> <a class="text-dark" href="<?php echo base_url("myAccount/index"); ?>"><i class="fas fa-user-circle"></i> <?php echo $this->session->userdata("user_fname"); ?></a></li>
+                                             <li> <a class="text-dark" href="<?php echo base_url("user/login_out/index/"); ?>">Logout</a></li>
+                                         <?php } ?>
+                                     <?php } ?>
                                  </ul>
                              </nav>
                          </div>
@@ -69,11 +73,11 @@
      </div>
      <!-- end header inner -->
  </header>
- <script type="text/javascript">                                     
-     function FuntionSearch(){
-         var hreff = "http://localhost/ci_project/Shop/Search/index/"+document.getElementById('keyword').value;
-         //alert(hreff);                 
-         location.replace(hreff);                                     
-     }
+ <script type="text/javascript">
 
- </script>  
+     function FuntionSearch() {
+         var hreff = "http://localhost/ci_project/Shop/Search/index/" + document.getElementById('keyword').value;
+         //alert(hreff);                 
+         location.replace(hreff);
+     }
+ </script>
