ea40cd65417cef92e30c44f4ff432cbdd510b3da
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-19 19:09:02 +0700

    delete load database

--- application/models/userModel.php
+++ application/models/userModel.php
@@ -8,7 +8,6 @@ class userModel extends CI_Model
     public function __construct()
     {
         parent::__construct();
-        $this->load->database();
     }
     public function index()
     {
