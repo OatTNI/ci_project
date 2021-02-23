595ade2ab773e383df8caff67dbf45b848c1765c
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-02-23 21:36:19 +0700

    แก้โชว์รูปโชว์ชื่อ

--- application/views/Admin/Management/ItemManager.php
+++ application/views/Admin/Management/ItemManager.php
@@ -14,11 +14,11 @@
 	<?php foreach ($Product as $P) { ?>
 	    <div class="col-xl-3 col-lg-3 col-ms-12">
 	        <div class="rounded-0 border text-center">
-	            <img class="card-img-top p-2 img-fluid" src="<?php echo $P->img; ?>" alt="Card image cap Responsive image" style="width: 200px; height:200px;">
+	            <img class="card-img-top p-2 img-fluid" src="<?php echo $P->image_url; ?>" alt="Card image cap Responsive image" style="width: 200px; height:200px;">
 	            <div class="card-body">
 	                <h5 class="card-title text-truncate"><strong><?php echo $P->product_name; ?></strong></h5>
 	                <p class="card-text text-truncate"><?php echo $P->description; ?></p>
-					<p class="card-text text-truncate"><?php echo $P->vendor; ?></p>
+					<p class="card-text text-truncate"><?php echo $P->vendor_name; ?></p>
 	                <a href="#" class="btn btn-warning">แก้ไข</a>
 	                <a href="#" class="btn btn-danger">ลบ</a>
 	            </div>
