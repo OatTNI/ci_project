3a07d1ca2569bf8d6b911e83de51f1783478707c
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-27 01:06:13 +0700

    hook for admin page

---
+++ application/hooks/only_admin.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class only_admin
+{
+    public function index()
+    {
+        $this->CI = &get_instance();
+        $classname = $this->CI->router->class;
+        
+        // ! class allowed for user doesn't login 
+        $allowed_class=[
+           "admin","addProduct","categoryManagement"
+        ];
+        $is_allow=false;
+        foreach($allowed_class as $class){
+            if($class==$classname){
+                $is_allow=true;
+            }
+        }
+        if($this->CI->session->userdata("user_id")){
+            if($this->CI->session->userdata("user_status")!=1&&$is_allow){
+                redirect("Home/index");
+            }
+        }
+    }
+}
