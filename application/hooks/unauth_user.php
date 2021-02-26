bb3c1e9079bae3d1a04105da8ae80bbd85b7689f
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-27 01:05:55 +0700

    change class name

---
+++ application/hooks/unauth_user.php
@@ -0,0 +1,28 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class unauth_user
+{
+    public function index()
+    {
+        $this->CI = &get_instance();
+        $classname = $this->CI->router->class;
+        
+        // ! class allowed for user doesn't login 
+        $allowed_class=[
+            "Home","Product","Products","Search","index","register","login_out"
+        ];
+        $is_allow=false;
+        foreach($allowed_class as $class){
+            if($class==$classname){
+                $is_allow=true;
+            }
+        }
+        if (!$this->CI->session->userdata('user_id')&&!$is_allow) {
+            redirect("Home/index");
+        }
+
+            
+    }
+}
