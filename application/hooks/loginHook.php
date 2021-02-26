43b06d3009b9111b6030d4deb0fd912a3ba30436
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-26 22:24:53 +0700

    checking class is allowed?

--- application/hooks/loginHook.php
+++ application/hooks/loginHook.php
@@ -8,8 +8,22 @@ class loginHook
     {
         $this->CI = &get_instance();
         $classname = $this->CI->router->class;
-        $methodname = $this->CI->router->method;
-        if (!$this->CI->session->userdata('user_id') && $methodname != "index" && $classname != "userCon") {
+
+        $allowed_class=[
+            "Home","Product","Products","Search","index","cartHome"
+        ];
+        $is_allow=false;
+        foreach($allowed_class as $class){
+            if($class==$classname){
+                $is_allow=true;
+            }
+        }
+        echo $classname;
+        echo $is_allow;
+
+        if (!$this->CI->session->userdata('user_id')&&!$is_allow) {
+            // echo $this->CI->session->userdata("user_id");
+            echo $classname;
             redirect("Home/index");
         }
     }
