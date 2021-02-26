a999a5a39b3b75aa7cb6738db74d943eaf9ff906
kwan19482 <62150121+kwan19482@users.noreply.github.com>
2021-02-26 20:32:45 +0700

    delete loginregister(old)

--- application/views/Admin/Management/CategoryManager.php
+++ application/views/Admin/Management/CategoryManager.php
@@ -22,8 +22,7 @@
 
 <div class="row ">
     <div class="col-8">
-        <a href="<?php echo base_url('admin/index'); ?> " class="btn btn-danger">
-            <a>BACK</a>
+        <a href="<?php echo base_url('admin/index'); ?> " class="btn btn-danger">BACK</a>
     </div>
     <div class="col-4">
         <h2 style="text-align: right; color:black;">จัดการหมวดหมู่</h2>
