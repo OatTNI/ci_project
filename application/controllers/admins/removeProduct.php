cd93a29f9a393256e9b00406ab32d1f168370f81
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-27 21:05:20 +0700

    add delete product

---
+++ application/controllers/admins/removeProduct.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class removeProduct extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Do your magic here
+        $this->load->model("Product_image_model");
+        $this->load->model("Product_model");
+    }
+
+    public function index($product_id="")
+    {
+        if($product_id!=""){
+            // $product_id=base64_decode($product_id);
+            $this->Product_image_model->delete_product_id($product_id);
+            $this->Product_model->delete($product_id);
+            redirect("admin/index");
+        }else{
+            redirect("My404/index");
+        }
+    }
+}
+
+/* End of file removeProduct.php */
