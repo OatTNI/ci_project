4208aca667b9855e5c309518e01fdaf059134116
kaoat <tanutchakornkhaisaeng@gmail.com>
2021-02-28 18:42:01 +0700

    add variables, loop for changing product detail

--- application/views/Admin/Management/AddItem.php
+++ application/views/Admin/Management/AddItem.php
@@ -1,4 +1,25 @@
-<div class="row ">
+<?php
+    if(isset($product_detail)){
+        $dest="admins/changeProduct/index";
+        $p_name=$product_detail->product_name;
+        $p_price=$product_detail->price;
+        $description=$product_detail->description;
+        $picture="";
+        $p_id=$product_detail->product_id;
+        $v_id=$product_detail->vendor_id;
+        $c_id=$product_detail->category_id;
+    }else{
+        $dest="admins/addProduct";
+        $p_name="";
+        $p_price="";
+        $description="";
+        $picture="";
+        $p_id="";
+        $v_id="";
+        $c_id="";
+        $images="";
+    }
+?><div class="row ">
     <div class="col-8">
         <a href="<?php echo base_url('admin/index'); ?> " class="btn btn-danger">
             BACK</a>
@@ -13,18 +34,18 @@
 <div class="dropdown-divider"></div>
 <div class="row">
     <div class="col">
-        <form action="<?php echo base_url("admins/addProduct"); ?>" method="post" name="AddItemForm">
+        <form action="<?php echo base_url($dest); ?>" method="post" name="AddItemForm">
             <table class="table table-borderless">
                 <tr>
                     <td><b>ชื่อสินค้า</b> <label style="color:red">*</label></td>
                     <td>
                         <?php echo form_error("ProductName"); ?>
-                        <input class="form-control mb-3" type="text" name="ProductName">
+                        <input value="<?php echo $p_name; ?>" class="form-control mb-3" type="text" name="ProductName">
                     </td>
                     <td><b>ราคาสินค้า</b> <label style="color:red"> * </label></td>
                     <td>
                         <?php echo form_error("ProductPrice"); ?>
-                        <input class="form-control mb-3" type="number" name="ProductPrice">
+                        <input value="<?php echo $p_price; ?>" class="form-control mb-3" type="number" name="ProductPrice">
                     </td>
                 </tr>
                 <tr>
@@ -34,7 +55,7 @@
                             <?php echo form_error("VendorName"); ?>
                             <select name="VendorName" class="form-control" id="VendorName">
                                 <?php foreach ($vendor as $v) { ?>
-                                    <option value="<?php echo $v->{'vendor_id'} ?>"><?php echo $v->{'vendor_name'} ?></option>
+                                    <option <?php if($v_id==$v->vendor_id){echo "selected";} ?> value="<?php echo $v->{'vendor_id'} ?>"><?php echo $v->{'vendor_name'} ?></option>
                                 <?php } ?>
                             </select>
                         </div>
@@ -45,7 +66,7 @@
                             <?php echo form_error("category"); ?>
                             <select name="category" class="form-control" id="Categoryinput">
                                 <?php foreach ($Category as $C) { ?>
-                                    <option value="<?php echo $C->{'category_id'} ?>"><?php echo $C->{'category_name'} ?></option>
+                                    <option <?php if($c_id==$C->category_id){echo "selected";} ?> value="<?php echo $C->{'category_id'} ?>"><?php echo $C->{'category_name'} ?></option>
                                 <?php } ?>
                             </select>
                         </div>
@@ -57,7 +78,15 @@
                     <td>
                         <span id="image">
                             <?php echo form_error("Picture[]"); ?>
-                            <input class="form-control mb-3" type="text" name="Picture[]"></span>
+                            <?php if($images==""){ ?>
+                            <input class="form-control mb-3" type="text" name="Picture[]">
+                            <?php }else{ 
+                                foreach($images as $image){ ?>
+                                    <input value="<?php echo $image->image_url; ?>" class="form-control mb-3" type="text" name="Picture[]">
+                                    <input type="hidden" name="Picture_ID[]" value="<?php echo $image->image_id; ?>">
+                                <?php }
+                            }?>
+                        </span>
                     </td>
                     <td colspan="2">
                         <a id="add_image" href="#" class="btn btn-outline-success">+ เพิ่มรูป</a>
@@ -69,7 +98,7 @@
                     <td><b>คำอธิบายสินค้า</b> <label style="color:red"> * </label></td>
                     <td colspan="3">
                         <?php echo form_error("description"); ?>
-                        <textarea class="form-control mb-3" rows="3" name="description"></textarea>
+                        <textarea class="form-control mb-3" rows="3" name="description"><?php echo $description; ?></textarea>
                     </td>
                 </tr>
                 <tr>
