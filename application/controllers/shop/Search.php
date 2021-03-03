91d866cee1d061fc05523102055e64c6680a603b
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-03-03 16:37:30 +0700

    Update Search.php

--- application/controllers/shop/Search.php
+++ application/controllers/shop/Search.php
@@ -13,32 +13,26 @@
 		}
 		public function index($key='')
     	{
-            if(strlen($key)>0){
-            $config['base_url'] = base_url("shop/Search/index/$key/");
-            $config['total_rows'] = $this->Product_model->count($key,0);
-            $config['per_page'] = 3;
-
-            $this->pagination->initialize($config);
-
-            $data['links'] =  $this->pagination->create_links();
-
-            $start = $this->uri->segment(5)>0?$this->uri->segment(5):0;
-            $product  = $this->Product_model->getProductsbySearchPerpage($start, $config['per_page'],$key);
-            $data['total_rows'] = $config['total_rows'];
-            $data['Product'] = $product;
-
-
-            $data['Keyword'] = $key;
-            $data['SeeAll'] = '';
-            $data['content'] = 'Shop/Shopmain';
-            $this->load->view('Shop', $data);
-        }
-        else{
-            redirect("shop/index");
-        }
         // $data['Product'] = $this->Product_model->getProductsbySearch($key);
 
-        
+        $config['base_url'] = base_url("shop/Search/index/$key/");
+        $config['total_rows'] = $this->Product_model->count($key,0);
+        $config['per_page'] = 3;
+
+        $this->pagination->initialize($config);
+
+        $data['links'] =  $this->pagination->create_links();
+
+        $start = $this->uri->segment(5)>0?$this->uri->segment(5):0;
+        $product  = $this->Product_model->getProductsbySearchPerpage($start, $config['per_page'],$key);
+        $data['total_rows'] = $config['total_rows'];
+        $data['Product'] = $product;
+
+
+        $data['Keyword'] = $key;
+        $data['SeeAll'] = '';
+        $data['content'] = 'Shop/Shopmain';
+        $this->load->view('Shop', $data);
     	}
 	}
 
