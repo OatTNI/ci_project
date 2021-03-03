fee936165d52dac51e0e56c28cee2ddfdb65ae2d
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-03-03 22:10:38 +0700

    Update Search.php

--- application/controllers/shop/Search.php
+++ application/controllers/shop/Search.php
@@ -1,39 +1,34 @@
-<?php 
-	/**
-	 * 
-	 */
-	class Search extends CI_Controller
-	{
-		
-		function __construct()
-		{
-			parent::__construct();
-			$this->load->model('Product_model');
+<?php
 
-		}
-		public function index($key='')
-    	{
-        // $data['Product'] = $this->Product_model->getProductsbySearch($key);
+/**
+ * 
+ */
+class Search extends CI_Controller
+{
 
-        $config['base_url'] = base_url("shop/Search/index/$key/");
-        $config['total_rows'] = $this->Product_model->count($key,0);
-        $config['per_page'] = 3;
-
-        $this->pagination->initialize($config);
-
-        $data['links'] =  $this->pagination->create_links();
-
-        $start = $this->uri->segment(5)>0?$this->uri->segment(5):0;
-        $product  = $this->Product_model->getProductsbySearchPerpage($start, $config['per_page'],$key);
-        $data['total_rows'] = $config['total_rows'];
-        $data['Product'] = $product;
-
-
-        $data['Keyword'] = $key;
-        $data['SeeAll'] = '';
-        $data['content'] = 'Shop/Shopmain';
-        $this->load->view('Shop', $data);
-    	}
-	}
-
- ?>
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Product_model');
+    }
+    public function index($key = '')
+    {
+        if (strlen($key) > 0) {
+            $config['base_url'] = base_url("shop/Search/index/$key/");
+            $config['total_rows'] = $this->Product_model->count($key, 0);
+            $config['per_page'] = 3;
+            $this->pagination->initialize($config);
+            $data['links'] =  $this->pagination->create_links();
+            $start = $this->uri->segment(5) > 0 ? $this->uri->segment(5) : 0;
+            $product  = $this->Product_model->getProductsbySearchPerpage($start, $config['per_page'], $key);
+            $data['total_rows'] = $config['total_rows'];
+            $data['Product'] = $product;
+            $data['Keyword'] = $key;
+            $data['SeeAll'] = '';
+            $data['content'] = 'Shop/Shopmain';
+            $this->load->view('Shop', $data);
+        } else {
+            redirect("shop/index");
+        }
+    }
+}
