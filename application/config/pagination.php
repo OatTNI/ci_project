076e4060064a452d730f3a79abfd64ca79f2c52e
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-03-03 22:06:13 +0700

    pagination

---
+++ application/config/pagination.php
@@ -0,0 +1,22 @@
+<?php
+
+$config['full_tag_open'] = '<ul class="pagination">';
+$config['full_tag_close'] = '</ul>';
+$config['attributes'] = ['class' => 'page-link'];
+$config['first_link'] = false;
+$config['last_link'] = false;
+$config['first_tag_open'] = '<li class="page-item">';
+$config['first_tag_close'] = '</li>';
+$config['prev_link'] = '&laquo';
+$config['prev_tag_open'] = '<li class="page-item">';
+$config['prev_tag_close'] = '</li>';
+$config['next_link'] = '&raquo';
+$config['next_tag_open'] = '<li class="page-item">';
+$config['next_tag_close'] = '</li>';
+$config['last_tag_open'] = '<li class="page-item">';
+$config['last_tag_close'] = '</li>';
+$config['cur_tag_open'] = '<li class="page-item active"><a href="#" class="page-link">';
+$config['cur_tag_close'] = '<span class="sr-only">(current)</span></a></li>';
+$config['num_tag_open'] = '<li class="page-item">';
+$config['num_tag_close'] = '</li>';
+// $config['attributes'] = array('class' => 'page-link');
--- application/views/Shop/ShopMain.php
+++ application/views/Shop/ShopMain.php
@@ -1,6 +1,7 @@
 <div class="row">
 
-    <div class="col-12 text-right mt-1">
+    <div class="col-10"></div>
+    <div class="col-2 align-content-end mt-1">
         <?php echo $links ?>
     </div>
 </div>
