f94f830f5839f58d6ae76ca20251f1d80b1c73f4
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-03-04 10:16:17 +0700

    Update navbar.php

--- application/views/layout/navbar.php
+++ application/views/layout/navbar.php
@@ -19,55 +19,56 @@
                         <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
 
                             <div class="row">
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 input-group">
-                                    <!-- Search -->
-                                    <input class="form-control form-control-sm rounded-left border" type="text" id="keyword" name="keyword" placeholder="What are you looking for?" aria-label="Search">
-                                    <button class="rounded-right bg-white mr-1" onclick="FuntionSearch()" style="width: 50px;"><i class="fas fa-search"></i></button>
-                                    <a class="btn btn-lg btn-primary" href="<?php echo base_url("cart/cartHome"); ?>" role="button"><i class="fas fa-shopping-cart"></i></a>
+                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                                    <div class="input-group">
+                                        <!-- Search -->
+                                        <input class="form-control form-control-sm rounded-left border" type="text" id="keyword" name="keyword" placeholder="What are you looking for?" aria-label="Search">
+                                        <button class="rounded-right bg-white mr-1" onclick="FunctionSearch()" style="width: 50px;"><i class="fas fa-search"></i></button>
+                                        <a class="btn btn-lg btn-primary" href="<?php echo base_url("cart/cartHome"); ?>" role="button"><i class="fas fa-shopping-cart"></i></a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
-                        <div class="menu-area">
-                            <div class="limit-box">
-                                <nav class="main-menu">
-                                    <ul class="menu-area-main">
-                                        <li> <a href="<?php echo base_url("Home/index"); ?>"><i class="fas fa-home"></i> Home</a> </li>
-                                        <li> <a href="<?php echo base_url("Shop/index"); ?>"><i class="fas fa-store"></i> Shop</a></li>
-                                        <?php
-                                        if (!$this->session->userdata("user_id")) { ?>
-                                            <!-- <li> 
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12">
+                            <div class="menu-area">
+                                <div class="limit-box">
+                                    <nav class="main-menu">
+                                        <ul class="menu-area-main">
+                                            <li> <a href="<?php echo base_url("Home/index"); ?>"><i class="fas fa-home"></i> Home</a> </li>
+                                            <li> <a href="<?php echo base_url("Shop/index"); ?>"><i class="fas fa-store"></i> Shop</a></li>
+                                            <?php
+                                            if (!$this->session->userdata("user_id")) { ?>
+                                                <!-- <li> 
                                             <a class="text-dark" id="loginbutton" data-toggle="modal" data-target="#LoginModal"><i class="fas fa-sign-in-alt"></i> Login</a>
                                             
                                          </li> -->
-                                            <li> <a href="<?php echo base_url("user/login_out/index"); ?>"><i class="fas fa-sign-in-alt"></i> Login</a></li>
-                                            <?php } else {
+                                                <li> <a href="<?php echo base_url("user/login_out/index"); ?>"><i class="fas fa-sign-in-alt"></i> Login</a></li>
+                                                <?php } else {
 
-                                            if ($this->session->userdata("user_status") == 1) { ?>
-                                                <li> <a href="<?php echo base_url("admin/index"); ?>"><i class="fas fa-toolbox"></i> ไปสู่แสงสว่าง</a></li>
-                                            <?php } else { ?>
-                                                <li> <a href="<?php echo base_url("myAccount/index"); ?>"><i class="fas fa-user-circle"></i> <?php echo $this->session->userdata("user_fname"); ?></a></li>
+                                                if ($this->session->userdata("user_status") == 1) { ?>
+                                                    <li> <a href="<?php echo base_url("admin/index"); ?>"><i class="fas fa-toolbox"></i> ไปสู่แสงสว่าง</a></li>
+                                                <?php } else { ?>
+                                                    <li> <a href="<?php echo base_url("myAccount/index"); ?>"><i class="fas fa-user-circle"></i> <?php echo $this->session->userdata("user_fname"); ?></a></li>
+                                                <?php } ?>
+                                                <li> <a href="<?php echo base_url("user/login_out/index/"); ?>"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                                             <?php } ?>
-                                            <li> <a href="<?php echo base_url("user/login_out/index/"); ?>"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                                        <?php } ?>
-                                    </ul>
-                                </nav>
+                                        </ul>
+                                    </nav>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- end header inner -->
+        <!-- end header inner -->
 </header>
 <!-- end header -->
 
 
 <script type="text/javascript">
     function FunctionSearch() {
-        var hreff = "http://localhost/ci_project/Shop/Search/index/"+document.getElementById('keyword').value;
+        var hreff = "http://localhost/ci_project/Shop/Search/index/" + document.getElementById('keyword').value;
         //alert(hreff);    
         //urldecode(hreff);
         location.replace(hreff);
