adae0962fb66c8ce105d82956bc147b1cbdd2aad
Jiramed-Jetawattana <je.jiramed_st@Tni.ac.th>
2021-03-07 19:48:03 +0700

    Update CategoryManager.php

--- application/views/Admin/Management/CategoryManager.php
+++ application/views/Admin/Management/CategoryManager.php
@@ -31,7 +31,7 @@
 <div class="dropdown-divider"></div>
 <div class="row">
     <div class="col mb-2">
-        <form action="<?= base_url("admins/categoryManagement/add")?>" method="post" name="AddCategoryForm" id="AddCategoryForm">
+        <form action="<?= base_url("admins/categoryManagement/add") ?>" method="post" name="AddCategoryForm" id="AddCategoryForm">
             <span class="HDText">Add Category</span>
             <div class="row">
                 <div class="col-9"><input class="form-control" type="text" name="catName" id="catName" placeholder="Category Name"></div>
@@ -40,26 +40,26 @@
         </form>
     </div>
 </div>
-<?php if($this->session->flashdata('flash_errors')){?>
+<?php if ($this->session->flashdata('flash_errors')) { ?>
     <div class="col-12 col-sm-6">
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        <div><?=$this->session->flashdata('flash_errors')?></div>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-        </div>
-    </div>
-    <?php } ?>
-    <?php if($this->session->flashdata('flash_success')){?>
-        <div class="col-12 col-sm-6">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <div><?=$this->session->flashdata('flash_success')?></div>
+            <div><?= $this->session->flashdata('flash_errors') ?></div>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-            </div>
         </div>
-<?php }?>
+    </div>
+<?php } ?>
+<?php if ($this->session->flashdata('flash_success')) { ?>
+    <div class="col-12 col-sm-6">
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div><?= $this->session->flashdata('flash_success') ?></div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </div>
+<?php } ?>
 <div class="dropdown-divider"></div>
 <div class="row">
     <div class="col">
@@ -71,16 +71,25 @@
                     <td class="text-center"><b>Product in Category</b></td>
                     <td></td>
                 </tr>
-                <?php $i=0; foreach ($categories as $category) { ?>
+                <?php $i = 0;
+                foreach ($categories as $category) { ?>
                     <tr>
                         <td><?php echo $category->category_id; ?></td>
-                        <td><label id="editText<?=$category->category_id;?>"><?php echo $category->category_name; ?></label>
-                        <input class="form-control editData" id="editInput<?=$category->category_id;?>" value="<?php echo $category->category_name; ?>"></td>
-                        <td class="text-center"><?php echo count($Product[$i]);?></td>
-                        <td style="float: right;"><a href="#" class="btn btn-warning editButton" id="editButton_<?=$category->category_id;?>">EDIT</a>&nbsp;
-                        <a href="#" class="btn btn-danger deleteButton" data-toggle="modal" data-target="#deleteModal<?=$category->category_id;?>" id="deleteButton_<?=$category->category_id;?>">DELETE</a></td>
+                        <td><label id="editText<?= $category->category_id; ?>"><?php echo $category->category_name; ?></label>
+                            <input class="form-control editData" id="editInput<?= $category->category_id; ?>" value="<?php echo $category->category_name; ?>">
+                        </td>
+                        <td class="text-center"><?php echo count($Product[$i]); ?></td>
+                        <td style="float: right;">
+                            <div class="row">
+                                <div class="col"><a href="#" class="btn btn-warning editButton" id="editButton_<?= $category->category_id; ?>">EDIT</a>&nbsp;
+                                </div>
+                                <div class="col"><a href="#" class="btn btn-danger deleteButton" data-toggle="modal" data-target="#deleteModal<?= $category->category_id; ?>" id="deleteButton_<?= $category->category_id; ?>">DELETE</a>
+                                </div>
+                            </div>
+                        </td>
                     </tr>
-                <?php $i++;} ?>
+                <?php $i++;
+                } ?>
             </table>
         </form>
     </div>
@@ -88,21 +97,21 @@
 
 <?php foreach ($categories as $category) { ?>
 
-<div class="modal fade" id="deleteModal<?=$category->category_id;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">ลบข้อมูล</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">ต้องการที่จะลบหรือไม่</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">ยกเลิก</button>
-                <button class="btn btn-primary" id="deleteConfirm<?=$category->category_id;?>">ลบ</button>
+    <div class="modal fade" id="deleteModal<?= $category->category_id; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">ลบข้อมูล</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">ต้องการที่จะลบหรือไม่</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">ยกเลิก</button>
+                    <button class="btn btn-primary" id="deleteConfirm<?= $category->category_id; ?>">ลบ</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
 <?php } ?>
